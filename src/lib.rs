32e9d78d3e69adca2485067dfaa8931070505143
Vivian-McKnight <McKnightVivian@hotmail.com>
2026-05-08 22:13:05 +1000

    Added Tile enum

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Tile {
+    Suit(Suit),
+    Honor(Honor),
+}
+
+/// suits each with associated number between 1 and 9
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Suit {
+    Characters(u8),
+    Circle(u8),
+    Bamboo(u8),
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Honor {
+    Wind(Wind),
+    Dragon(Dragon),
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Wind {
+    East,
+    South,
+    West,
+    North,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Dragon {
+    Red,
+    Green,
+    White,
+}
