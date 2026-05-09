a64342078cfab8b4f643bd9ea9876dc9d6c94526
skoove <zie@skoove.dev>
2026-05-09 18:46:45 +1000

    render: add animation controls

--- src/tile/render.rs
+++ src/tile/render.rs
@@ -27,6 +27,10 @@ pub struct TileMaterial {
     overlay_texture: Option<Handle<Image>>,
     #[uniform(4)]
     tint: LinearRgba,
+    #[uniform(5)]
+    tilt: Vec2,
+    #[uniform(6)]
+    scale: Vec2,
 }
 
 impl TileMaterial {
@@ -40,11 +44,51 @@ impl TileMaterial {
             front_texture,
             back_texture,
             overlay_texture,
+            tilt: Vec2::ZERO,
+            scale: Vec2::ONE,
         }
     }
 
+    // tint setters and getters
     pub fn set_tint(&mut self, tint: impl Into<LinearRgba>) {
         self.tint = tint.into();
+    }
+
+    pub fn get_tint(self) -> LinearRgba {
+        self.tint
+    }
+
+    #[must_use]
+    pub fn get_tint_mut(&mut self) -> &mut LinearRgba {
+        &mut self.tint
+    }
+
+    // scale getters and setters
+    pub fn set_scale(&mut self, scale: &Vec2) {
+        self.scale = *scale
+    }
+
+    pub fn get_scale(self) -> Vec2 {
+        self.scale
+    }
+
+    #[must_use]
+    pub fn get_scale_mut(&mut self) -> &mut Vec2 {
+        &mut self.scale
+    }
+
+    // tilt getters and setters
+    pub fn set_tilt(&mut self, tilt: &Vec2) {
+        self.tilt = *tilt
+    }
+
+    pub fn get_tilt(self) -> Vec2 {
+        self.tilt
+    }
+
+    #[must_use]
+    pub fn get_tilt_mut(&mut self) -> &mut Vec2 {
+        &mut self.tilt
     }
 }
 
